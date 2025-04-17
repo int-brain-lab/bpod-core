15c2b1a11e148ceb7455f9efc7db2c7ac5cd6e30
Florian Rau <bimac@users.noreply.github.com>
2025-04-17 13:57:32 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,8 @@
 import os
 import sys
+from datetime import date
 
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath('../..'))
 from bpod_core import __version__
 
 # Configuration file for the Sphinx documentation builder.
@@ -12,9 +13,9 @@ from bpod_core import __version__
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "bpod_core"
-copyright = "2025, International Brain Laboratory"
-author = "International Brain Laboratory"
+project = 'bpod-core'
+copyright = f'{date.today().year}, International Brain Laboratory'
+author = 'International Brain Laboratory'
 release = '.'.join(__version__.split('.')[:3])
 version = '.'.join(__version__.split('.')[:3])
 rst_prolog = f"""
@@ -25,18 +26,18 @@ rst_prolog = f"""
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "myst_parser",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.napoleon",
+    'myst_parser',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.inheritance_diagram',
-    "sphinx_copybutton",
-    "sphinx_autodoc_typehints",
+    'sphinx_copybutton',
+    'sphinx_autodoc_typehints',
 ]
-source_suffix = [".rst", ".md"]
+source_suffix = ['.rst', '.md']
 
-templates_path = ["_templates"]
+templates_path = ['_templates']
 exclude_patterns = []
 
 typehints_defaults = None
@@ -45,24 +46,24 @@ typehints_use_signature = False
 typehints_use_signature_return = False
 
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/3.10/", None),
-    "numpy": ("http://docs.scipy.org/doc/numpy/", None),
-    "serial": ("https://pyserial.readthedocs.io/en/stable/", None),
+    'python': ('https://docs.python.org/3.10/', None),
+    'numpy': ('http://docs.scipy.org/doc/numpy/', None),
+    'serial': ('https://pyserial.readthedocs.io/en/stable/', None),
 }
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 
 # -- Settings for automatic API generation -----------------------------------
-autodoc_mock_imports = ["_typeshed"]
-autodoc_class_signature = 'separated'  # 'mixed', 'separated'
-autodoc_member_order = 'groupwise'  # 'alphabetical', 'groupwise', 'bysource'
+autodoc_mock_imports = ['_typeshed']
+autodoc_class_signature = 'separated'         # 'mixed', 'separated'
+autodoc_member_order = 'groupwise'            # 'alphabetical', 'groupwise', 'bysource'
 autodoc_inherit_docstrings = False
-autodoc_typehints = 'description'  # 'description', 'signature', 'none', 'both'
+autodoc_typehints = 'description'             # 'description', 'signature', 'none', 'both'
 autodoc_typehints_description_target = 'all'  # 'all', 'documented', 'documented_params'
-autodoc_typehints_format = 'short'  # 'fully-qualified', 'short'
+autodoc_typehints_format = 'short'            # 'fully-qualified', 'short'
 
 autosummary_generate = True
 autosummary_imported_members = False
