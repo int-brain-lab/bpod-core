d7a7dd02cad8be1897a5b156c56e6bb62d5255eb
Florian Rau <bimac@users.noreply.github.com>
2025-08-04 19:49:24 +0100

    Update test_bpod.py

--- tests/test_bpod.py
+++ tests/test_bpod.py
@@ -124,6 +124,8 @@ def mock_bpod_20(mock_comports, mock_ext_serial):  # noqa: ARG001
         patch('bpod_core.bpod.ExtendedSerial', return_value=mock_ext_serial),
         patch('bpod_core.bpod.Bpod._detect_additional_serial_ports'),
         patch('bpod_core.bpod.ZMQService'),
+        patch('bpod_core.bpod.json.load', return_value={}),
+        patch('bpod_core.bpod.json.dump', return_value={})
     ):
         yield Bpod
 
@@ -135,6 +137,8 @@ def mock_bpod_25(mock_comports, mock_ext_serial):  # noqa: ARG001
         patch('bpod_core.bpod.ExtendedSerial', return_value=mock_ext_serial),
         patch('bpod_core.bpod.Bpod._detect_additional_serial_ports'),
         patch('bpod_core.bpod.ZMQService'),
+        patch('bpod_core.bpod.json.load', return_value={}),
+        patch('bpod_core.bpod.json.dump', return_value={})
     ):
         yield Bpod
 
@@ -146,6 +150,8 @@ def mock_bpod_2p(mock_comports, mock_ext_serial):  # noqa: ARG001
         patch('bpod_core.bpod.ExtendedSerial', return_value=mock_ext_serial),
         patch('bpod_core.bpod.Bpod._detect_additional_serial_ports'),
         patch('bpod_core.bpod.ZMQService'),
+        patch('bpod_core.bpod.json.load', return_value={}),
+        patch('bpod_core.bpod.json.dump', return_value={})
     ):
         yield Bpod
 
