363352f16a52ff4edf0ca1aca01e4c8cf0b33b64
Florian Rau <bimac@users.noreply.github.com>
2025-08-29 00:03:48 +0100

    add logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -131,14 +131,15 @@ intersphinx_mapping = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_logo = "_static/bpod-core.svg"
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
+    'logo_only': True,
     'collapse_navigation': True,
     'sticky_navigation': True,
     'navigation_depth': 4,
     'includehidden': True,
     'titles_only': False,
-    'display_version': True,
 }
 
 # -- Settings for automatic API generation -----------------------------------
