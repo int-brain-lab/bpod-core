6a9d7dcdf6cf751539c481bf4194fb2246995704
Florian Rau <bimac@users.noreply.github.com>
2026-04-05 12:58:36 +0100

    Add format string constants for struct packing

--- bpod_core/constants.py
+++ bpod_core/constants.py
@@ -3,22 +3,40 @@
 from enum import IntEnum
 from struct import Struct
 
+# struct format strings
+FMT_UINT8 = 'B'
+"""Format string for an unsigned 8-bit integer."""
+FMT_UINT16_LE = '<H'
+"""Format string for an unsigned 16-bit integer (little-endian)."""
+FMT_UINT32_LE = '<I'
+"""Format string for an unsigned 32-bit integer (little-endian)."""
+FMT_UINT64_LE = '<Q'
+"""Format string for an unsigned 64-bit integer (little-endian)."""
+FMT_INT8 = 'b'
+"""Format string for a signed 8-bit integer."""
+FMT_INT16_LE = '<h'
+"""Format string for a signed 16-bit integer (little-endian)."""
+FMT_INT32_LE = '<i'
+"""Format string for a signed 32-bit integer (little-endian)."""
+FMT_INT64_LE = '<q'
+"""Format string for a signed 64-bit integer (little-endian)."""
+
 # pre-compiled structs for common data types
-STRUCT_UINT8 = Struct('B')
+STRUCT_UINT8 = Struct(FMT_UINT8)
 """Compiled struct representing an unsigned 8-bit integer."""
-STRUCT_UINT16_LE = Struct('<H')
+STRUCT_UINT16_LE = Struct(FMT_UINT16_LE)
 """Compiled struct representing an unsigned 16-bit integer (little-endian)."""
-STRUCT_UINT32_LE = Struct('<I')
+STRUCT_UINT32_LE = Struct(FMT_UINT32_LE)
 """Compiled struct representing an unsigned 32-bit integer (little-endian)."""
-STRUCT_UINT64_LE = Struct('<Q')
+STRUCT_UINT64_LE = Struct(FMT_UINT64_LE)
 """Compiled struct representing an unsigned 64-bit integer (little-endian)."""
-STRUCT_INT8 = Struct('b')
+STRUCT_INT8 = Struct(FMT_INT8)
 """Compiled struct representing a signed 8-bit integer."""
-STRUCT_INT16_LE = Struct('<h')
+STRUCT_INT16_LE = Struct(FMT_INT16_LE)
 """Compiled struct representing a signed 16-bit integer (little-endian)."""
-STRUCT_INT32_LE = Struct('<i')
+STRUCT_INT32_LE = Struct(FMT_INT32_LE)
 """Compiled struct representing a signed 32-bit integer (little-endian)."""
-STRUCT_INT64_LE = Struct('<q')
+STRUCT_INT64_LE = Struct(FMT_INT64_LE)
 """Compiled struct representing a signed 64-bit integer (little-endian)."""
 
 IPV4_LOOPBACK = '127.0.0.1'
