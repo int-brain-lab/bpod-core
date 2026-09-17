3066135da70c7b78aa670e7091194c39f1dd1473
Florian Rau <bimac@users.noreply.github.com>
2026-09-18 00:17:59 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -327,8 +327,8 @@ llms_txt_exclude = ['schema']
 llms_txt_full_build = True
 
 # we only set `markdown_http_base` if we're running on GitHub
-if os.getenv("GITHUB_ACTIONS") == "true":
-    markdown_http_base = "https://int-brain-lab.github.io/bpod-core/"
+if os.getenv('GITHUB_ACTIONS') == 'true':
+    markdown_http_base = 'https://int-brain-lab.github.io/bpod-core/'
 
 # -- Miscellaneous -------------------------------------------------------------
 
